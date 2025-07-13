e63c2c4e51a388cac7df0cd84eaff1498bcaf70b
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-07-13 00:24:51 -0500

    [Feat] Updates * Update to 1.21.7 * Cleanup unused template files. * stop advertising for "another-mod"

--- src/client/java/bannerboats/BannerBoatsClient.java
+++
@@ -1,9 +0,0 @@
-package bannerboats;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class BannerBoatsClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
--- src/main/java/bannerboats/mixin/AbstractBoatEntityMixin.java
+++ src/main/java/bannerboats/mixin/AbstractBoatEntityMixin.java
@@ -2,6 +2,10 @@ package bannerboats.mixin;
 
 import bannerboats.attachment.ModAttachments;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.AbstractBoatEntity;
 import net.minecraft.entity.vehicle.VehicleEntity;
@@ -9,31 +13,33 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractBoatEntity.class)
 public abstract class AbstractBoatEntityMixin extends VehicleEntityMixin {
-    @Unique
-    private static final ThreadLocal<ItemStack> BANNER = ThreadLocal.withInitial(() -> null);
+	@Unique
+	private static final ThreadLocal<ItemStack> BANNER = ThreadLocal.withInitial(() -> null);
 
-    public AbstractBoatEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
-    }
+	public AbstractBoatEntityMixin(EntityType<?> type, World world) {
+		super(type, world);
+	}
 
-    @Override
-    protected void afterDropSelf(ServerWorld world, Item item, CallbackInfo ci) {
-        super.afterDropSelf(world, item, ci);
-        var stack = BANNER.get();
-        if (stack != null)
-            dropStack(world, stack);
-    }
+	@Override
+	protected void afterDropSelf(ServerWorld world, Item item, CallbackInfo ci) {
+		super.afterDropSelf(world, item, ci);
+		var stack = BANNER.get();
+		if (stack != null)
+			dropStack(world, stack);
+	}
 
-    @Override
-    protected void wrapDropSelf(VehicleEntity instance, ServerWorld world, Item item, Operation<Void> original) {
-        ModAttachments.getBanner(instance).ifPresent(BANNER::set);
-        original.call(instance, world, item);
-        BANNER.remove();
-    }
+	@Override
+	protected void wrapDropSelf(VehicleEntity instance, ServerWorld world, Item item, Operation<Void> original) {
+		var o = BANNER.get();
+		try {
+			ModAttachments.getBanner(instance).ifPresent(BANNER::set);
+			original.call(instance, world, item);
+		} finally {
+			if (o != null) BANNER.set(o);
+			else BANNER.remove();
+		}
+	}
 }
